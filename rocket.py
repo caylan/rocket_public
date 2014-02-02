bda312bbb01c4cc1b700a3ff8c74e71818dbe737
Caylan Lee <caylanlee@gmail.com>
2014-02-02 01:07:53 -0800

    Added file for db model declaration, a little bit of logic on routing

--- rocket.py
+++ rocket.py
@@ -1,9 +1,10 @@
 import os
-from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
 from flask.ext.sqlalchemy import SQLAlchemy
+
+import rocket_models
 
 app = Flask(__name__)
 
@@ -21,17 +22,43 @@ db = SQLAlchemy(app)
 def index():
     return redirect(url_for('login'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return "This will be the login page"
+    if request.method == 'POST':
+        # Do a login, might be different if we use flask-login
+        pass
+    else:
+        # Render login form
+        pass
+    return "This will be the login page"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Process the form and return user to login page
+        pass
+    else:
+        # Render a user registration form
+        pass
+    return "This will be a registration form"
 
 @app.route('/jobs')
 def show_jobs():
-	return "This will show the user a list of jobs, based on their qualifications"
+    """
+    Renders a list of jobs that the currently logged in user qualifies for.
+    """
+    # All this code is scratch, dependent on the sqlaclhemy yet do be written
+    user = flask.ext.login.current_user
+    # How do we filter the jobs, all job tags must be fulfilled by user?
+    jobs = Job.query.filter_by(tags=user.tags)
+
+    return "This will show the user a list of jobs, based on their qualifications"
 
 @app.route('/details/<int:jid>')
-def display_details(jid):
-	return "This will display details about the given job with id = " + str(jid)
+def display_job_details(jid):
+    job = Job.query.get(jid)
+    return "Job with id: " + jid + ", name: " + job.name + "location: " \
+            + ", deadline: " + job.deadline + ", description: " + job.description
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
---
+++ rocket_models.py
@@ -0,0 +1,4 @@
+# Define the models for use in the rocket database here. 
+# Use Flask-SQLAlchemy declarations, examples found here:
+# http://pythonhosted.org/Flask-SQLAlchemy/models.html
+
