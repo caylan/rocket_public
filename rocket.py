b981c8f271a86a8d09911c532480f4cdc9dc5c14
Caylan Lee <caylanlee@gmail.com>
2014-02-03 15:37:16 -0800

    added boilerplate templates

--- rocket.py
+++ rocket.py
@@ -4,8 +4,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 
 from flask.ext.sqlalchemy import SQLAlchemy
 
-import rocket_models
-
+## Config setup
 app = Flask(__name__)
 
 # Load default config
@@ -18,6 +17,29 @@ app.config.update(dict(
 
 db = SQLAlchemy(app)
 
+
+## This will be moved to a separate file, probably
+from datetime import datetime
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    location = db.Column(db.String(80))
+    deadline = db.Column(db.DateTime)
+    description = db.Column(db.Text)
+
+    def __init__(self, name, location, deadline, description):
+        self.name = name
+        self.location = location
+        self.deadline = deadline
+        self.description = description
+
+    def __repr__(self):
+        return '<Job %r | %r>' % (self.name, self.id)
+
+## Load db models
+# import rocket_models
+
+## Routing
 @app.route('/')
 def index():
     return redirect(url_for('login'))
@@ -30,6 +52,8 @@ def login():
     else:
         # Render login form
         pass
+
+    return render_template('login.html')
     return "This will be the login page"
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -48,11 +72,22 @@ def show_jobs():
     Renders a list of jobs that the currently logged in user qualifies for.
     """
     # All this code is scratch, dependent on the sqlaclhemy yet do be written
-    user = flask.ext.login.current_user
+    # user = flask.ext.login.current_user
     # How do we filter the jobs, all job tags must be fulfilled by user?
-    jobs = Job.query.filter_by(tags=user.tags)
+    # jobs = Job.query.filter_by(tags=user.tags)
 
-    return "This will show the user a list of jobs, based on their qualifications"
+    j1 = Job('Job 1', 'Seattle, WA', datetime.now(), 'This is a description of the first job')
+    j1.id = 1
+    j2 = Job('Job 2', 'Spokane, WA', datetime.now(), 'This is a description of the second job')
+    j2.id = 2
+    j3 = Job('Job 3', 'Portland, OR', datetime.now(), 'This is a description of the third job')
+    j3.id = 3
+
+    jobs = [j1, j2, j3]
+
+    return render_template('job_list.html', jobs=jobs)
+
+    # return "This will show the user a list of jobs, based on their qualifications"
 
 @app.route('/details/<int:jid>')
 def display_job_details(jid):
--- rocket_models.py
+++ rocket_models.py
@@ -2,3 +2,22 @@
 # Use Flask-SQLAlchemy declarations, examples found here:
 # http://pythonhosted.org/Flask-SQLAlchemy/models.html
 
+from datetime import datetime
+
+# Note: Job will not contain a reference to Organization, 
+# Organization will contain a backref to Job
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    location = db.Column(db.String(80))
+    deadline = db.Column(db.DateTime)
+    description = db.Column(db.Text)
+
+    def __init__(self, name, location, deadline, description):
+        self.name = name
+        self.location = location
+        self.deadline = deadline
+        self.description = description
+
+    def __repr__(self):
+    	return '<Job %r | %r>' % (self.name, self.id)
